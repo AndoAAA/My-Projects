75b18d940af8b38c629f09ad4f63a0b22319b6b0
AndoAAA <tarverdyan070@gmail.com>
2024-08-28 18:27:17 +0400

    fix

--- src/components/Success/Success.jsx
+++ src/components/Success/Success.jsx
@@ -8,7 +8,7 @@ export default function Success({count}) {
       <div className='success-container'>
         <img src={IMAGES.successImg} alt="success-img" className='success-img' />
         <h3>Success!</h3>
-        <p>All users send {count} invitation</p>
+        <p>Sent {count} invitation</p>
         <button onClick={()=> window.location.reload()} className='back-btn'>Back to Home</button>
       </div>
     </>
