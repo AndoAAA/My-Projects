c9eeaf83f69ebfdf0ec87fcc97f590183d27e4cb
AndoAAA <tarverdyan070@gmail.com>
2024-08-29 16:34:03 +0400

    use-converter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import Block from "./Block/Block";
 
 function App() {
+  const [rates, setRates] = useState({});
+  const [currency1, setCurrency1] = useState("AMD");
+  const [currency2, setCurrency2] = useState("USD");
+  const [currency1Price, setCurrency1Price] = useState(1);
+  const [currency2Price, setCurrency2Price] = useState(1);
+
+  useEffect(() => {
+    fetch("https://www.cbr-xml-daily.ru/daily_json.js")
+      .then((res) => res.json())
+      .then((json) => {
+        setRates(json.rates);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const onChangePrice1 = (value) => {
+    const price = value / rates[currency1];
+    const result = price * rates[currency2];
+    setCurrency2Price(result);
+    setCurrency1Price(value);
+  };
+
+  const onChangePrice2 = (value) => {
+    const result = (rates[currency1] / rates[currency2]) * value;
+    setCurrency1(result);
+    setCurrency2Price(value);
+  };
+
+  useEffect(() => {
+    onChangePrice1(currency1Price);
+  }, [currency1]);
+
+  useEffect(() => {
+    onChangePrice2(currency2Price);
+  }, [currency2]);
+
   return (
     <>
-      My Project
+      <div className="container">
+        <Block
+          value={0}
+          currency={currency1}
+          onChangeCurrency={setCurrency1}
+          onChangeValue={onChangePrice1}
+        />
+        <Block
+          value={0}
+          currency={currency2}
+          onChangeCurrency={setCurrency2}
+          onChangeValue={onChangePrice2}
+        />
+      </div>
     </>
   );
 }
---
+++ src/Block/Block.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const defaultCurrencies = ['AMD','RUB','USD','EUR'];
+
+export default function Block({value, currency, onChangeValue, onChangeCurrency}) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
