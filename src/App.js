0cf8eaac8ce1de516d001cb0bd0dfa7e2fe2ccf6
AndoAAA <tarverdyan070@gmail.com>
2024-09-08 08:51:07 +0400

    Photo-Collection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./index.scss";
+import Collection from "./components/Collection";
+
+const categories = [
+  { name: "Все" },
+  { name: "Море" },
+  { name: "Горы" },
+  { name: "Архитектура" },
+  { name: "Города" },
+];
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [categoryID, setCategoryID] = useState(0);
+  const [searchValue, setSearchValue] = useState("");
+  const [collections, setCollections] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `"https://66dc505a47d749b72acb471f.mockapi.io/photos"${
+        categoryID ? `category=${categoryID}` : ""
+      }`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        setCollections(json);
+      })
+      .catch((err) => {
+        console.log(err);
+      }).finally(()=> setIsLoading(false));
+  }, [categoryID]);
+
   return (
-    <>
-      My Project
-    </>
+    <div className="App">
+      <h1>Моя коллекция фотографий</h1>
+      <div className="top">
+        <ul className="tags">
+          {categories.map((obj, index) => (
+            <li
+              onClick={() => setCategoryID(index)}
+              className={categoryID === index ? "active" : ""}
+              key={obj.name}
+            >
+              {obj.name}
+            </li>
+          ))}
+        </ul>
+        <input
+          value={searchValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          className="search-input"
+          placeholder="Поиск по названию"
+        />
+      </div>
+      <div className="content">
+        {isLoading ? (
+          <h2>Loading ...</h2>
+        ) : (
+          collections
+            .filter((obj) => {
+              return obj.name.toLowerCase().includes(searchValue.toLowerCase());
+            })
+            .map((obj, index) => (
+              <Collection key={index} name={obj.name} images={obj.photos} />
+            ))
+        )}
+      </div>
+      <ul className="pagination">
+        <li>1</li>
+        <li className="active">2</li>
+        <li>3</li>
+      </ul>
+    </div>
   );
 }
 
