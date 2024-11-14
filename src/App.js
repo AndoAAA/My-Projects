0998214ddb9c90179b6ac3c2753f7e1ad688ca85
AndoAAA <tarverdyan070@gmail.com>
2024-11-14 20:37:41 +0400

    creat-PizzaBlock

--- src/App.js
+++ src/App.js
@@ -3,16 +3,27 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/db.json")
+      .then((resp) => resp.json())
+      .then((json) => { 
+        setPizzas(json.pizzas || []); 
+      });
+  }, []);
+
   return (
     <>
       <div className="wrapper">
         <Header />
         <div className="content">
           <Routes>
-            <Route path="/" element={<Home />} exact />
-            <Route path="/cart" element={<Cart/>}/>
+            <Route path="/" element={<Home items={pizzas} />} exact />
+            <Route path="/cart" element={<Cart />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
